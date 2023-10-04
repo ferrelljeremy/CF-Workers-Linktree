8dd52a0ff191fcc7d8ef565e887d7359b8142318
ferrelljeremy <31968419+ferrelljeremy@users.noreply.github.com>
2023-10-04 07:11:41 -0500

    Create getImages.js
    to add imaged below the social links

---
+++ dao/getImages.js
@@ -0,0 +1,14 @@
+// dao/getImages.js
+var images = [
+  {
+    name: "Image 1",
+    url: "https://example.com/image1.jpg"
+  },
+  {
+    name: "Image 2",
+    url: "https://example.com/image2.jpg"
+  },
+  // Add more image objects as needed
+];
+
+module.exports = images;
